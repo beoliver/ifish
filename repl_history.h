884b363be7b45ba4c0e053bf7219c1f954f395a6
Benjamin Edward Oliver <benjamin.e.oliver@gmail.com>
2015-10-23 16:14:43 +0200

    Update repl_history.h

--- repl_history.h
+++ repl_history.h
@@ -2,9 +2,19 @@
 /* not 100% decided on return types */
 
 void repl_history_init();
+/* allocate memory for the history struct */
 
 int repl_history_insert(char*);
+/* try to insert a line into history */
+/* return 1 on success, 0 on failure */
+
+int repl_history_get(int, char*);
+/* try to copy the Nth most recent line from history into the buffer passed */
+/* return 1 on success, 0 on failure */
 
 int repl_history_delete(int);
+/* try to delete the Nth most recent lines from history */
+/* return 1 on success, 0 on failure */
 
 int repl_history_teardown();
+/* free memory etc... */
