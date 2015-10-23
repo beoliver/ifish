479d5ac96fb77fe64d58a16c80ac9ebc62b9d58e
Benjamin Edward Oliver <benjamin.e.oliver@gmail.com>
2015-10-23 15:55:27 +0200

    Update repl_history.c

--- repl_history.c
+++ repl_history.c
@@ -1,11 +1,3 @@
 #include "repl_history.h"
 
-/* declare all functions static apart from the following */
-/* not 100% decided on return types */
-
-void repl_history_init();
-
-int repl_history_insert(char*);
-
-int repl_history_delete(int);
 
