0df9333bdd0bcf9fab409ae04a36b80764b3ade8
Benjamin Edward Oliver <benjamin.e.oliver@gmail.com>
2015-10-24 21:50:41 +0200

    added real parsing.h
    reused an original function

--- repl_parsing.c
+++ repl_parsing.c
@@ -1,1 +1,58 @@
+
+#include <string.h>
+#include <stdlib.h>
+
+#include <sys/stat.h>
 #include "repl_parsing.h"
+
+
+char* search_user_path(char* param) {
+  
+  char* paths         = getenv("PATH");
+  char* start_path_pt = paths;
+  char* end_path_pt   = paths;
+  int   path_length   = 0;
+
+  /* If there are '/' file separators in the line, we treat */
+  /* the line as a direct call. We return a copy of it      */
+
+  if (index(param, '/') != NULL) {
+    return strdup(param);
+  }
+  
+  while (1) {
+        
+    if ((*end_path_pt == ':') || (*end_path_pt == '\0')) {
+      char* path = malloc(sizeof(char) * (path_length + 2 + strlen(param)));
+
+      if (path == NULL) {
+	perror("char* search_user_path(char* param) :");
+	return NULL;
+      }
+      
+      stpncpy(path, start_path_pt, path_length);
+      strcat(path, "/");
+      strcat(path, param);
+
+      /* test if executable exists */
+      struct stat fileStat;
+      if (stat(path, &fileStat) >= 0) {
+	return path;
+      } 
+
+      if (*end_path_pt == 0) {
+	/* end of paths, no match was found */
+	return NULL;
+      } else {
+	/* keep searching... */
+	/* move pointer to reference beginning of next path */
+	end_path_pt++;
+	start_path_pt = end_path_pt;
+	path_length = 0;
+      }
+    } else {
+      end_path_pt++;
+      path_length++;
+    }
+  }
+}
---
+++ repl_parsing.h
@@ -0,0 +1,10 @@
+
+#ifndef __REPL_PARSING__
+#define __REPL_PARSING__
+
+#include <string.h>
+#include <errno.h>
+
+char* search_user_path(char*);
+
+#endif
